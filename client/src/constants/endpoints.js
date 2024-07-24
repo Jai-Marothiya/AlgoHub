d585fb08454806e27ea49a11ef84184dc5f00886
Jai-Marothiya <jaimarothiya48@gmail.com>
2024-07-24 21:46:28 +0530

    fix endpoints

--- client/src/constants/endpoints.js
+++ client/src/constants/endpoints.js
@@ -1,5 +1,5 @@
 // const baseURL = `https://temp.lessonpal.com`;
-const baseURL = `https://algohub-taya.netlify.app/`;
+const baseURL = `https://algohub-taya.netlify.app`;
 // const baseURL = `http://localhost:5000`;
 // const baseURL = `http://192.168.1.4:3000`;
 // export const graphqlApiURL = `${baseURL}/graphql`;
